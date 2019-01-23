4f32a119c9ae0fb8ea61c2a15b062dbaf9a06491
BAEK Y. JEONG <byjeong@us.ibm.com>
2019-01-23 17:04:31 -0600

    fvt enablement

---
+++ test/fvt/item.list.js
@@ -0,0 +1,14 @@
+module.exports = {
+  'Demo test Google' : function (client) {
+    require('../../utils/setenv')
+    console.log('#### global.doiUrls = ', global.doiUrls)
+    client
+      .url(global.doiUrls.uiServer || 'http://localhost:3010')
+      .waitForElementVisible('body', 5000)
+      .assert.title('Microservices Sample')
+      .waitForElementVisible('div.item:first-child', 10000)
+      .assert.containsText('div.item:first-child',
+        'Monitor')
+      .end();
+  }
+}
