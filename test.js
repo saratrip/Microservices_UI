2f5e247455579cd05c2882a8cf0ea19daa29191e
BAEK Y. JEONG <byjeong@us.ibm.com>
2019-01-25 12:27:47 -0600

    use chromedriver

--- test.js
+++ test.js
@@ -6,17 +6,10 @@ describe('Checkout Google.com', function () {
     it('Search on Google', function() {
 
 
-        var webdriver = require('selenium-webdriver');
-        var chrome = require('selenium-webdriver/chrome');
-        var path = require('chromedriver').path;
-        
-        var service = new chrome.ServiceBuilder(path).build();
-        chrome.setDefaultService(service);
-        
-        var driver = new webdriver.Builder()
+        const webdriver = require('selenium-webdriver');
+        const driver = new webdriver.Builder()
             .withCapabilities(webdriver.Capabilities.chrome())
             .build();
-        
         
         driver.get('http://www.google.com/ncr');
         driver.findElement(By.name('q')).sendKeys('webdriver', Key.RETURN);
