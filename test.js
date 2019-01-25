d75346aca4c4fffd06e1ce65a12f534cc2d6a09d
BAEK Y. JEONG <byjeong@us.ibm.com>
2019-01-25 11:57:53 -0600

    use selenium-webdriver 3.6

--- test.js
+++ test.js
@@ -1,62 +1,19 @@
-// test.js
-// Import requirement packages
 require('chromedriver');
 const assert = require('assert');
-const {Builder, Key, By, until} = require('selenium-webdriver');
-// describe('Checkout Google.com', function () {
-//     let driver;
-//     before(async function() {
-//         driver = await new Builder().forBrowser('chrome').build();
-//     });
-//     // Next, we will write steps for our test. 
-//     // For the element ID, you can find it by open the browser inspect feature.
-//     it('Search on Google', async function() {
-//         // Load the page
-//         await driver.get('https://google.com');
-//         // Find the search box by id
-//         await driver.findElement(By.name('q')).click();
-//         // Enter keywords and click enter
-//         await driver.findElement(By.name('q')).sendKeys('wikipedia', Key.RETURN);
-//         // Wait for the results box by id
-//         await driver.wait(until.elementLocated(By.id('rcnt')), 10000);
-//         // We will get the title value and test it
-//         let title = await driver.getTitle();
-//         assert.equal(title, 'wikipedia - Google Search');
-//     });
-//     // close the browser after running tests
-//     after(() => driver && driver.quit());
-// })
-
-
-
-// client
-// .url(global.doiUrls.uiServer || 'http://localhost:3010')
-// .waitForElementVisible('body', 5000)
-// .assert.title('Microservices Sample')
-// .waitForElementVisible('div.item:first-child', 10000)
-// .assert.containsText('div.item:first-child',
-//   'Monitor')
-// .end();
-// }
-
+const {Builder, By, Key, until} = require('selenium-webdriver');
 
 describe('Checkout Google.com', function () {
     it('Search on Google', function() {
-        let driver = new Builder().forBrowser('chrome').build();
-        // Load the page
-        driver.get('https://google.com');
-        // Find the search box by id
-        driver.findElement(By.name('q')).click();
-        // Enter keywords and click enter
-        driver.findElement(By.name('q')).sendKeys('wikipedia', Key.RETURN);
-        // Wait for the results box by id
-        driver.wait(until.elementLocated(By.id('rcnt')), 10000);
-        // We will get the title value and test it
-        let title = driver.getTitle();
-        setTimeout(() => {
-            assert.equal(title, 'wikipedia - Google Search');
-            driver.quit();
-        }, 3000)
+
+        let driver = new Builder()
+            .forBrowser('chrome')
+            .build();
+        
+        driver.get('http://www.google.com/ncr');
+        driver.findElement(By.name('q')).sendKeys('webdriver', Key.RETURN);
+        driver.wait(until.titleIs('webdriver - Google Search'), 1000);
+        driver.quit();
+        
     })
     
 })
